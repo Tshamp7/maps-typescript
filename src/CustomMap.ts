225959c5b3dae9148f94916b9661fe89abfe41b3
Thomas Shamp <T.shamp7@gmail.com>
2021-02-17 12:45:16 -0800

    add ability to add markers to the map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,20 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById("map"), {
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(objectWithLocation: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: objectWithLocation.location,
     });
   }
 }
